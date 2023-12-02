1a8b740b6f04f4e10a713189d767be9428ea3eaa
= <horup@deepstacker.com>
2023-12-02 21:24:27 +0100

    wip

--- src/systems.rs
+++ src/systems.rs
@@ -1,10 +1,30 @@
+use macroquad::prelude::*;
 use crate::Context;
 
 
-pub fn once(c:&mut Context) {
+fn start(c:&mut Context) {
 
 }
 
-pub fn tick(c:&mut Context) {
+pub fn once(c:&mut Context) {
+    let systems = [
+        start
+    ];
 
+    for system in systems.iter() {
+        system(c);
+    }
+}
+
+pub fn draw(c:&mut Context) {
+    draw_rectangle(0.0, 0.0, screen_width(), screen_height(), DARKGRAY);
+}
+
+pub fn tick(c:&mut Context) {
+    let systems = [
+        draw
+    ];
+    for system in systems.iter() {
+        system(c);
+    }
 }
