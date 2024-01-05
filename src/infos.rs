8227a31f8293d4ff3cba642e3d64c1c8295b5a19
Søren Alsbjerg Hørup <sah@sah.dk>
2024-01-05 09:51:33 +0100

    wip

--- src/infos.rs
+++ src/infos.rs
@@ -1,12 +1,16 @@
 use std::{collections::HashMap, rc::Rc};
 
 use glam::Vec2;
-use macroquad::{file::load_file, texture::Texture2D};
-use toml::Table;
+use macroquad::{
+    file::load_file,
+    texture::{load_texture, Texture2D, Image},
+};
+use toml::{Table, Value};
 
+use crate::FrameIndex;
 
 /// Extends the table with content from a 'super' table.
-/// 
+///
 /// Assumes a key called 'extends' which points to a table from which it can extend
 fn extend_table(mut tables: Table) -> Table {
     let names: Vec<String> = tables.keys().map(|x| x.to_owned()).collect();
@@ -33,11 +37,18 @@ fn extend_table(mut tables: Table) -> Table {
 }
 
 /// Load table from path and extend it using the `extend_table` function
-async fn load_and_extend_table(path:&str) -> Table {
-    let actors = String::from_utf8(load_file(path).await.unwrap()).unwrap();
-    let table: toml::Table = toml::from_str(&actors).unwrap();
+async fn load_and_extend_table(path: &str) -> Table {
+    let table = load_table(path).await;
     extend_table(table)
 }
+
+/// Load table from path
+async fn load_table(path: &str) -> Table {
+    let table = String::from_utf8(load_file(path).await.unwrap()).unwrap();
+    toml::from_str(&table).unwrap()
+}
+
+type InfoCollection<T> = HashMap<String, Rc<T>>;
 
 pub struct ImageInfo2 {
     pub name: String,
@@ -46,29 +57,165 @@ pub struct ImageInfo2 {
 }
 
 #[derive(Clone)]
-pub struct FrameIndex2 {
+pub struct ImageIndex {
     pub image: Rc<ImageInfo2>,
     pub frame: u16,
 }
 
+async fn load_images(table: &Table) -> InfoCollection<ImageInfo2> {
+    let mut map = HashMap::default();
+    for (name, value) in table.iter() {
+        let Some(path) = value.as_str() else { continue; };
+        let texture = load_texture(&("assets/".to_owned() + path)).await.expect("failed to load texture");
+        map.insert(
+            name.to_owned(),
+            Rc::new(ImageInfo2 {
+                name: name.to_owned(),
+                path: path.to_owned(),
+                texture,
+            }),
+        );
+    }
+    map
+}
 
 #[derive(Clone, Default)]
 pub struct WeaponInfo2 {
+    pub name:String,
     pub rate_of_fire: f32,
-    pub name: String,
-    pub frames: Vec<FrameIndex2>,
+    pub frames: Vec<ImageIndex>,
     pub damage: [f32; 2],
     pub mount_offset: f32,
     pub muzzle_offset: f32,
-    pub spread:f32
+    pub spread: f32,
+}
+
+fn get_f32(prop: &str, props: &Value) -> Option<f32> {
+    let Some(v) = props.get(prop) else {
+        return None;
+    };
+    v.as_float()
+        .or(v.as_integer().map(|x| x as f64))
+        .map(|x| x as f32)
+}
+fn get_array_string(prop: &str, props: &Value) -> Option<Vec<String>> {
+    let Some(v) = props.get(prop) else {
+        return None;
+    };
+
+    let mut res = Vec::new();
+    let Some(v) = v.as_array() else {
+        return None;
+    };
+    for v in v.iter() {
+        let Some(v) = v.as_str() else {
+            return None;
+        };
+        res.push(v.to_string());
+    }
+
+    Some(res)
+}
+
+fn get_bool(prop: &str, props: &Value) -> Option<bool> {
+    let Some(v) = props.get(prop) else {
+        return None;
+    };
+    v.as_bool()
+}
+
+fn get_str<'a>(prop: &'a str, props: &'a Value) -> Option<&'a str> {
+    let Some(v) = props.get(prop) else {
+        return None;
+    };
+    v.as_str()
+}
+
+fn get_array_f32<'a>(prop: &'a str, props: &'a Value) -> Option<Vec<f32>> {
+    let Some(v) = props.get(prop) else {
+        return None;
+    };
+    let Some(v) = v.as_array() else {
+        return None;
+    };
+    let mut vec = Vec::new();
+    for v in v.iter() {
+        match v {
+            Value::Integer(i) => vec.push(*i as f32),
+            Value::Float(f) => vec.push(*f as f32),
+            _ => {}
+        }
+    }
+    return Some(vec);
+}
+
+fn get_vec2<'a>(prop: &'a str, props: &'a Value) -> Option<Vec2> {
+    let Some(v) = get_array_f32(prop, props) else {
+        return None;
+    };
+    if v.len() == 2 {
+        return Some(Vec2::new(v[0], v[1]));
+    }
+
+    return None;
+}
+
+fn get_tuple_f32<'a>(prop: &'a str, props: &'a Value) -> Option<(f32, f32)> {
+    let Some(v) = get_array_f32(prop, props) else {
+        return None;
+    };
+    if v.len() == 2 {
+        return Some((v[0], v[1]));
+    }
+
+    return None;
+}
+
+fn get_frames<'a>(prop: &'a str, props: &'a Value, images:&InfoCollection<ImageInfo2>) -> Vec<ImageIndex> {
+    let mut frames = Vec::new();
+    if let Some(props_frames) = get_array_string("frames", props) {
+        for frame in props_frames.iter() {
+            frames.push(ImageIndex {
+                image: images.get(frame).expect("could not find image").clone(),
+                frame: 0,
+            });
+            
+        }
+    }
+    frames
+}
+
+async fn load_weapons(table:&Table, images:&InfoCollection<ImageInfo2>) -> HashMap<String, Rc<WeaponInfo2>> {
+    let mut map = InfoCollection::default();
+    
+    for (name, props) in table.iter() {
+        let damage = match get_array_f32("damage", &props) {
+            Some(damage) => [
+                damage.get(0).copied().unwrap_or_default(),
+                damage.get(1).copied().unwrap_or_default(),
+            ],
+            None => [0.0, 0.0],
+        };
+
+        map.insert(name.to_owned(), Rc::new(WeaponInfo2 {
+            name: name.to_owned(),
+            rate_of_fire: get_f32("rate_of_fire", props).unwrap_or_default(),
+            frames: get_frames("frames", props, images),
+            damage: damage,
+            mount_offset: get_f32("mount_offset", props).unwrap_or_default(),
+            muzzle_offset: get_f32("muzzle_offset", props).unwrap_or_default(),
+            spread: get_f32("spread", props).unwrap_or_default(),
+        }));
+    }
+    map
 }
 
 #[derive(Clone)]
 pub struct ActorInfo2 {
     pub name: String,
-    pub frames: Vec<FrameIndex2>,
-    pub locomotion_frames: Vec<FrameIndex2>,
-    pub dead_frames: Vec<FrameIndex2>,
+    pub frames: Vec<ImageIndex>,
+    pub locomotion_frames: Vec<ImageIndex>,
+    pub dead_frames: Vec<ImageIndex>,
     pub bot: bool,
     pub speed: f32,
     pub radius: f32,
@@ -88,12 +235,16 @@ pub struct ActorInfo2 {
 }
 
 pub struct Infos {
-    
+    // pub images:HashMap<String, ImageInfo2>
+    //pub actors:HashMap<String, ActorInfo2>
 }
 impl Infos {
     pub async fn init() -> Self {
-        let actors = load_and_extend_table("assets/actors.toml").await;
+        let images = load_table("assets/images.toml").await;
+        let images = load_images(&images).await;
         let weapons = load_and_extend_table("assets/weapons.toml").await;
+        let weapons = load_weapons(&weapons, &images).await;
+        let actors = load_and_extend_table("assets/actors.toml").await;
         Infos {}
     }
 }
