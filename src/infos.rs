0d601979438ebb10fa646af44e505f854d18dad9
Søren Alsbjerg Hørup <sah@sah.dk>
2024-01-05 08:58:06 +0100

    wip

--- src/infos.rs
+++ src/infos.rs
@@ -1,6 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, rc::Rc};
 
-use macroquad::file::load_file;
+use glam::Vec2;
+use macroquad::{file::load_file, texture::Texture2D};
 use toml::Table;
 
 
@@ -31,26 +32,59 @@ fn extend_table(mut tables: Table) -> Table {
     return tables;
 }
 
+/// Load table from path and extend it using the `extend_table` function
 async fn load_and_extend_table(path:&str) -> Table {
     let actors = String::from_utf8(load_file(path).await.unwrap()).unwrap();
     let table: toml::Table = toml::from_str(&actors).unwrap();
     extend_table(table)
 }
 
-type InfoIndex = u16;
-
-pub trait Info {
-    fn name(&self) -> &str;
-    fn index(&self) -> InfoIndex;
+pub struct ImageInfo2 {
+    pub name: String,
+    pub path: String,
+    pub texture: Texture2D,
 }
 
-pub struct InfoCollection<T> {
-    inner: Vec<T>,
-    name_to_index: HashMap<String, InfoIndex>,
+#[derive(Clone)]
+pub struct FrameIndex2 {
+    pub image: Rc<ImageInfo2>,
+    pub frame: u16,
 }
 
-impl<T:Info> InfoCollection<T> {
-    
+
+#[derive(Clone, Default)]
+pub struct WeaponInfo2 {
+    pub rate_of_fire: f32,
+    pub name: String,
+    pub frames: Vec<FrameIndex2>,
+    pub damage: [f32; 2],
+    pub mount_offset: f32,
+    pub muzzle_offset: f32,
+    pub spread:f32
+}
+
+#[derive(Clone)]
+pub struct ActorInfo2 {
+    pub name: String,
+    pub frames: Vec<FrameIndex2>,
+    pub locomotion_frames: Vec<FrameIndex2>,
+    pub dead_frames: Vec<FrameIndex2>,
+    pub bot: bool,
+    pub speed: f32,
+    pub radius: f32,
+    pub missile: bool,
+    pub shootable: bool,
+    pub health: f32,
+    pub solid: bool,
+    pub particle: bool,
+    /// current active weapon
+    pub weapon: Rc<WeaponInfo2>,
+    /// frame offset from center of actor
+    pub offset: Vec2,
+    /// rotate the frame such that it faces facing
+    pub rotate_to_face: bool,
+    pub missile_direct_damage: (f32, f32),
+    pub missile_splash_damage: (f32, f32),
 }
 
 pub struct Infos {
