9f050e1aa1063f5d55756db2c1ce5be9a25a86df
Søren Alsbjerg Hørup <sah@sah.dk>
2024-01-05 11:24:55 +0100

    wip

--- src/infos.rs
+++ src/infos.rs
@@ -7,45 +7,6 @@ use macroquad::{
 };
 use toml::{Table, Value};
 
-/// Extends the table with content from a 'super' table.
-///
-/// Assumes a key called 'extends' which points to a table from which it can extend
-fn extend_table(mut tables: Table) -> Table {
-    let names: Vec<String> = tables.keys().map(|x| x.to_owned()).collect();
-    for name in names.iter() {
-        let mut final_table = Table::default();
-        let org_table = tables.get(name).unwrap().as_table().unwrap();
-        if let Some(extends_table) = org_table
-            .get("extends")
-            .and_then(|x| x.as_str())
-            .and_then(|x| tables.get(x))
-            .and_then(|x| x.as_table())
-        {
-            final_table = extends_table.clone();
-        }
-
-        for (key, value) in org_table.iter() {
-            final_table.insert(key.clone(), value.clone());
-        }
-
-        tables.insert(name.clone(), toml::Value::Table(final_table));
-    }
-
-    return tables;
-}
-
-/// Load table from path and extend it using the `extend_table` function
-async fn load_and_extend_table(path: &str) -> Table {
-    let table = load_table(path).await;
-    extend_table(table)
-}
-
-/// Load table from path
-async fn load_table(path: &str) -> Table {
-    let table = String::from_utf8(load_file(path).await.unwrap()).unwrap();
-    toml::from_str(&table).unwrap()
-}
-
 type InfoCollection<T> = HashMap<String, Rc<T>>;
 
 pub struct ImageInfo2 {
@@ -60,32 +21,46 @@ pub struct ImageIndex {
     pub frame: u16,
 }
 
-async fn load_images(table: &Table) -> InfoCollection<ImageInfo2> {
-    let mut map = HashMap::default();
-    for (name, value) in table.iter() {
-        let Some(path) = value.as_str() else { continue; };
-        let texture = load_texture(&("assets/".to_owned() + path)).await.expect("failed to load texture");
-        map.insert(
-            name.to_owned(),
-            Rc::new(ImageInfo2 {
-                name: name.to_owned(),
-                path: path.to_owned(),
-                texture,
-            }),
-        );
-    }
-    map
-}
-
 #[derive(Clone, Default)]
 pub struct WeaponInfo2 {
-    pub name:String,
+    pub name: String,
     pub rate_of_fire: f32,
     pub frames: Vec<ImageIndex>,
     pub damage: [f32; 2],
     pub mount_offset: f32,
     pub muzzle_offset: f32,
     pub spread: f32,
+}
+
+#[derive(Clone)]
+pub struct ActorInfo2 {
+    pub name: String,
+    pub frames: Vec<ImageIndex>,
+    pub locomotion_frames: Vec<ImageIndex>,
+    pub dead_frames: Vec<ImageIndex>,
+    pub bot: bool,
+    pub speed: f32,
+    pub radius: f32,
+    pub missile: bool,
+    pub shootable: bool,
+    pub health: f32,
+    pub solid: bool,
+    pub particle: bool,
+    /// current active weapon
+    pub weapon: Rc<WeaponInfo2>,
+    /// frame offset from center of actor
+    pub offset: Vec2,
+    /// rotate the frame such that it faces facing
+    pub rotate_to_face: bool,
+    pub missile_direct_damage: (f32, f32),
+    pub missile_splash_damage: (f32, f32),
+}
+
+#[derive(Default)]
+pub struct Infos {
+    pub images: InfoCollection<ImageInfo2>,
+    pub weapons: InfoCollection<WeaponInfo2>,
+    pub actors: InfoCollection<ActorInfo2>,
 }
 
 fn get_f32(prop: &str, props: &Value) -> Option<f32> {
@@ -169,7 +144,11 @@ fn get_tuple_f32<'a>(prop: &'a str, props: &'a Value) -> Option<(f32, f32)> {
     return None;
 }
 
-fn get_frames<'a>(prop: &'a str, props: &'a Value, images:&InfoCollection<ImageInfo2>) -> Vec<ImageIndex> {
+fn get_frames<'a>(
+    prop: &'a str,
+    props: &'a Value,
+    images: &InfoCollection<ImageInfo2>,
+) -> Vec<ImageIndex> {
     let mut frames = Vec::new();
     if let Some(props_frames) = get_array_string("frames", props) {
         for frame in props_frames.iter() {
@@ -177,13 +156,73 @@ fn get_frames<'a>(prop: &'a str, props: &'a Value, images:&InfoCollection<ImageI
                 image: images.get(frame).expect("could not find image").clone(),
                 frame: 0,
             });
-            
         }
     }
     frames
 }
 
-async fn load_weapons(table:&Table, images:&InfoCollection<ImageInfo2>) -> HashMap<String, Rc<WeaponInfo2>> {
+/// Extends the table with content from a 'super' table.
+///
+/// Assumes a key called 'extends' which points to a table from which it can extend
+fn extend_table(mut tables: Table) -> Table {
+    let names: Vec<String> = tables.keys().map(|x| x.to_owned()).collect();
+    for name in names.iter() {
+        let mut final_table = Table::default();
+        let org_table = tables.get(name).unwrap().as_table().unwrap();
+        if let Some(extends_table) = org_table
+            .get("extends")
+            .and_then(|x| x.as_str())
+            .and_then(|x| tables.get(x))
+            .and_then(|x| x.as_table())
+        {
+            final_table = extends_table.clone();
+        }
+
+        for (key, value) in org_table.iter() {
+            final_table.insert(key.clone(), value.clone());
+        }
+
+        tables.insert(name.clone(), toml::Value::Table(final_table));
+    }
+
+    return tables;
+}
+
+/// Load table from path and extend it using the `extend_table` function
+async fn load_and_extend_table(path: &str) -> Table {
+    let table = load_table(path).await;
+    extend_table(table)
+}
+
+/// Load table from path
+async fn load_table(path: &str) -> Table {
+    let table = String::from_utf8(load_file(path).await.unwrap()).unwrap();
+    toml::from_str(&table).unwrap()
+}
+
+async fn load_images(table: &Table) -> InfoCollection<ImageInfo2> {
+    let mut map = HashMap::default();
+    for (name, value) in table.iter() {
+        let Some(path) = value.as_str() else { continue; };
+        let texture = load_texture(&("assets/".to_owned() + path))
+            .await
+            .expect("failed to load texture");
+        map.insert(
+            name.to_owned(),
+            Rc::new(ImageInfo2 {
+                name: name.to_owned(),
+                path: path.to_owned(),
+                texture,
+            }),
+        );
+    }
+    map
+}
+
+async fn load_weapons(
+    table: &Table,
+    images: &InfoCollection<ImageInfo2>,
+) -> HashMap<String, Rc<WeaponInfo2>> {
     let mut map = InfoCollection::default();
     map.insert("".to_string(), Rc::new(WeaponInfo2::default()));
     for (name, props) in table.iter() {
@@ -195,76 +234,60 @@ async fn load_weapons(table:&Table, images:&InfoCollection<ImageInfo2>) -> HashM
             None => [0.0, 0.0],
         };
 
-        map.insert(name.to_owned(), Rc::new(WeaponInfo2 {
-            name: name.to_owned(),
-            rate_of_fire: get_f32("rate_of_fire", props).unwrap_or_default(),
-            frames: get_frames("frames", props, images),
-            damage: damage,
-            mount_offset: get_f32("mount_offset", props).unwrap_or_default(),
-            muzzle_offset: get_f32("muzzle_offset", props).unwrap_or_default(),
-            spread: get_f32("spread", props).unwrap_or_default(),
-        }));
+        map.insert(
+            name.to_owned(),
+            Rc::new(WeaponInfo2 {
+                name: name.to_owned(),
+                rate_of_fire: get_f32("rate_of_fire", props).unwrap_or_default(),
+                frames: get_frames("frames", props, images),
+                damage: damage,
+                mount_offset: get_f32("mount_offset", props).unwrap_or_default(),
+                muzzle_offset: get_f32("muzzle_offset", props).unwrap_or_default(),
+                spread: get_f32("spread", props).unwrap_or_default(),
+            }),
+        );
     }
     map
 }
 
-
-async fn load_actors(table:&Table, images:&InfoCollection<ImageInfo2>, weapons:&InfoCollection<WeaponInfo2>) -> InfoCollection<ActorInfo2> {
+async fn load_actors(
+    table: &Table,
+    images: &InfoCollection<ImageInfo2>,
+    weapons: &InfoCollection<WeaponInfo2>,
+) -> InfoCollection<ActorInfo2> {
     let mut map = InfoCollection::default();
     for (name, props) in table.iter() {
-        map.insert(name.to_owned(), Rc::new(ActorInfo2 {
-            name: name.to_owned(),
-            frames: get_frames("frames", props, images),
-            locomotion_frames: get_frames("locomotion_frames", props, images),
-            dead_frames: get_frames("dead_frames", props, images),
-            bot: get_bool("bot", props).unwrap_or_default(),
-            speed: get_f32("speed", props).unwrap_or_default(),
-            radius: get_f32("radius", props).unwrap_or_default(),
-            missile: get_bool("missile", props).unwrap_or_default(),
-            shootable: get_bool("shootable", props).unwrap_or_default(),
-            health: get_f32("health", props).unwrap_or_default(),
-            solid: get_bool("solid", props).unwrap_or_default(),
-            particle: get_bool("particle", props).unwrap_or_default(),
-            weapon: weapons.get(get_str("weapon", props).unwrap_or_default()).expect("could not find weapon").clone(),
-            offset: get_vec2("offset", props).unwrap_or_default(),
-            rotate_to_face: get_bool("rotate_to_face", props).unwrap_or_default(),
-            missile_direct_damage: get_tuple_f32("missile_direct_damage", props).unwrap_or_default(),
-            missile_splash_damage: get_tuple_f32("missile_splash_damage", props).unwrap_or_default(),
-        }));
+        map.insert(
+            name.to_owned(),
+            Rc::new(ActorInfo2 {
+                name: name.to_owned(),
+                frames: get_frames("frames", props, images),
+                locomotion_frames: get_frames("locomotion_frames", props, images),
+                dead_frames: get_frames("dead_frames", props, images),
+                bot: get_bool("bot", props).unwrap_or_default(),
+                speed: get_f32("speed", props).unwrap_or_default(),
+                radius: get_f32("radius", props).unwrap_or_default(),
+                missile: get_bool("missile", props).unwrap_or_default(),
+                shootable: get_bool("shootable", props).unwrap_or_default(),
+                health: get_f32("health", props).unwrap_or_default(),
+                solid: get_bool("solid", props).unwrap_or_default(),
+                particle: get_bool("particle", props).unwrap_or_default(),
+                weapon: weapons
+                    .get(get_str("weapon", props).unwrap_or_default())
+                    .expect("could not find weapon")
+                    .clone(),
+                offset: get_vec2("offset", props).unwrap_or_default(),
+                rotate_to_face: get_bool("rotate_to_face", props).unwrap_or_default(),
+                missile_direct_damage: get_tuple_f32("missile_direct_damage", props)
+                    .unwrap_or_default(),
+                missile_splash_damage: get_tuple_f32("missile_splash_damage", props)
+                    .unwrap_or_default(),
+            }),
+        );
     }
     map
 }
 
-#[derive(Clone)]
-pub struct ActorInfo2 {
-    pub name: String,
-    pub frames: Vec<ImageIndex>,
-    pub locomotion_frames: Vec<ImageIndex>,
-    pub dead_frames: Vec<ImageIndex>,
-    pub bot: bool,
-    pub speed: f32,
-    pub radius: f32,
-    pub missile: bool,
-    pub shootable: bool,
-    pub health: f32,
-    pub solid: bool,
-    pub particle: bool,
-    /// current active weapon
-    pub weapon: Rc<WeaponInfo2>,
-    /// frame offset from center of actor
-    pub offset: Vec2,
-    /// rotate the frame such that it faces facing
-    pub rotate_to_face: bool,
-    pub missile_direct_damage: (f32, f32),
-    pub missile_splash_damage: (f32, f32),
-}
-
-#[derive(Default)]
-pub struct Infos {
-    pub images:InfoCollection<ImageInfo2>,
-    pub weapons:InfoCollection<WeaponInfo2>,
-    pub actors:InfoCollection<ActorInfo2>
-}
 impl Infos {
     pub async fn new() -> Self {
         let images = load_table("assets/images.toml").await;
@@ -276,7 +299,7 @@ impl Infos {
         Infos {
             images,
             weapons,
-            actors
+            actors,
         }
     }
 }
